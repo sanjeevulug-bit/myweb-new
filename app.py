d2f4e5cc1de63861e61789c3739b2fde82c0dc57
sanjeevulug-bit <sanjeevulug@gmail.com>
2026-04-07 13:36:16 +0530

    Prepare app for deployment

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello from Docker 🚀")
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+PORT = 10000
+
+Handler = SimpleHTTPRequestHandler
+
+with HTTPServer(("", PORT), Handler) as httpd:
+    print("Server running...")
+    httpd.serve_forever()
